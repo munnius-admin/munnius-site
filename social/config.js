c04a1e0b3cbd91d8515f22d4590ecf2c9ac89c71
Munnius Admin <admin@munnius.com.br>
2026-07-28 22:09:24 -0300

    Connect Munnius Social to Supabase

--- social/config.js
+++ social/config.js
@@ -1,2 +1,5 @@
 // Configuração pública do frontend. Não coloque senhas nem a service_role aqui.
-window.MUNNIUS_SOCIAL_CONFIG = {};
+window.MUNNIUS_SOCIAL_CONFIG = {
+  supabaseUrl: "https://fyjerbxkjxlxtccyutyn.supabase.co",
+  supabaseAnonKey: "sb_publishable_CMxzNmKp5GwKODEU_Jx74Q_4RNKmtaD",
+};
